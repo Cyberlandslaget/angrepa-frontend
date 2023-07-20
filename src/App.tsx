dfacd6b7b00ef91b8e54b866d234a6af3328ed00
Shirajuki <shirajuki00@gmail.com>
2023-07-20 14:10:04 +0200

    Add fullscreen functionality on the log displays

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,16 @@ import { useState } from 'react';
 
 function App() {
   const [pin, setPin] = useState('simple');
-  const [fullscreen, setFullscreen] = useState(false);
+  const [fullscreen, setFullscreen] = useState('');
 
   const updatePin = (display: 'simple' | 'runner' | 'submission') => {
     console.log(display);
     setPin(display);
+  };
+
+  const updateFullscreen = (display: 'simple' | 'runner' | 'submission') => {
+    if (fullscreen === display) setFullscreen('');
+    else setFullscreen(display);
   };
 
   return (
@@ -24,9 +29,17 @@ function App() {
       <div
         className={`tertiaryColor w-full h-full p-2 rounded-md overflow-auto ${
           pin === 'simple' ? '!order-1 [grid-column:span_2]' : 'order-3'
+        } ${
+          fullscreen === 'simple'
+            ? 'absolute z-20 mt-[3.5rem] top-3 left-3 [width:calc(100%-1.5rem)] [height:calc(100%-2.25rem-2.75rem)]'
+            : ''
         }`}
       >
-        <div className="secondaryColor sticky flex top-0 w-[6.25rem] h-6 -my-1 z-10 -translate-y-2 -translate-x-2 rounded-br-md text-xs gap-1 p-1 items-center">
+        <div
+          className={`secondaryColor sticky flex top-0 w-[6.25rem] h-6 -my-1 z-10 -translate-y-2 -translate-x-2 rounded-br-md text-xs gap-1 p-1 items-center ${
+            pin === 'simple' ? 'w-[7.25rem]' : ''
+          }`}
+        >
           <button
             className={`px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all ${
               pin === 'simple' ? '!bg-black cursor-default' : ''
@@ -35,7 +48,12 @@ function App() {
           >
             {pin === 'simple' ? 'Pinned' : 'Pin'}
           </button>
-          <button className="px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all">
+          <button
+            className={`px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all ${
+              fullscreen === 'simple' ? '!bg-black' : ''
+            }`}
+            onClick={() => updateFullscreen('simple')}
+          >
             Fullscreen
           </button>
         </div>
@@ -50,9 +68,17 @@ function App() {
       <div
         className={`tertiaryColor w-full h-full p-2 rounded-md overflow-auto overflow-x-hidden order-2 ${
           pin === 'runner' ? '!order-1 [grid-column:span_2]' : ''
+        } ${
+          fullscreen === 'runner'
+            ? 'absolute z-20 mt-[3.5rem] top-3 left-3 [width:calc(100%-1.5rem)] [height:calc(100%-2.25rem-2.75rem)]'
+            : ''
         }`}
       >
-        <div className="secondaryColor sticky flex top-0 w-[6.25rem] h-6 -my-1 z-10 -translate-y-2 -translate-x-2 rounded-br-md text-xs gap-1 p-1 items-center">
+        <div
+          className={`secondaryColor sticky flex top-0 w-[6.25rem] h-6 -my-1 z-10 -translate-y-2 -translate-x-2 rounded-br-md text-xs gap-1 p-1 items-center ${
+            pin === 'runner' ? 'w-[7.25rem]' : ''
+          }`}
+        >
           <button
             className={`px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all ${
               pin === 'runner' ? '!bg-black cursor-default' : ''
@@ -61,7 +87,12 @@ function App() {
           >
             {pin === 'runner' ? 'Pinned' : 'Pin'}
           </button>
-          <button className="px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all">
+          <button
+            className={`px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all ${
+              fullscreen === 'runner' ? '!bg-black' : ''
+            }`}
+            onClick={() => updateFullscreen('runner')}
+          >
             Fullscreen
           </button>
         </div>
@@ -71,9 +102,17 @@ function App() {
       <div
         className={`tertiaryColor w-full h-full p-2 rounded-md overflow-auto order-4 ${
           pin === 'submission' ? '!order-1 [grid-column:span_2]' : ''
+        } ${
+          fullscreen === 'submission'
+            ? 'absolute z-20 mt-[3.5rem] top-3 left-3 [width:calc(100%-1.5rem)] [height:calc(100%-2.25rem-2.75rem)]'
+            : ''
         }`}
       >
-        <div className="secondaryColor sticky flex top-0 w-[6.25rem] h-6 -my-1 z-10 -translate-y-2 -translate-x-2 rounded-br-md text-xs gap-1 p-1 items-center">
+        <div
+          className={`secondaryColor sticky flex top-0 w-[6.25rem] h-6 -my-1 z-10 -translate-y-2 -translate-x-2 rounded-br-md text-xs gap-1 p-1 items-center ${
+            pin === 'submission' ? 'w-[7.25rem]' : ''
+          }`}
+        >
           <button
             className={`px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all ${
               pin === 'submission' ? '!bg-black cursor-default' : ''
@@ -82,7 +121,12 @@ function App() {
           >
             {pin === 'submission' ? 'Pinned' : 'Pin'}
           </button>
-          <button className="px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all">
+          <button
+            className={`px-2 bg-slate-950 bg-opacity-60 text-[0.6rem] rounded-sm hover:bg-black transition-all ${
+              fullscreen === 'submission' ? '!bg-black' : ''
+            }`}
+            onClick={() => updateFullscreen('submission')}
+          >
             Fullscreen
           </button>
         </div>
