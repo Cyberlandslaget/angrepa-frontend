e2eb70aa8fb3404495c314eae5777a98a02b899e
Shirajuki <shirajuki00@gmail.com>
2023-07-22 17:48:58 +0200

    Update populate script

--- polling/populate.py
+++ polling/populate.py
@@ -2,6 +2,10 @@ import requests
 from utils import parse
 
 SERVER_URL = "http://localhost:5000/api/scoreboard"
+import requests
+
+ips = requests.get('https://7.enowars.com/api/data/ips').text.split('\n')
+teamss = requests.get('https://7.enowars.com/api/data/teams').json()
 teams = [
 	{"id": 1, "name": "organizers"},
 	{"id": 2, "name": "fibonhack"},
@@ -9,18 +13,25 @@ teams = [
 	{"id": 4, "name": "printer toner level: 90%"},
 	{"id": 5, "name": "bootplug"},
 ]
+teams = []
+
+for ip, team in zip(ips, teamss['confirmedTeams']):
+  teamId = ip.split(".")[2]
+  teams.append({"id": int(teamId), "name": team['name']})
+  print(ip, team['name'])
 data_teams = {}
 
 for t in teams:
 	data_team = {
 		"name": t["name"],
 		"services": {
-			"dummy-web1": parse("up"),
-			"dummy-web2": parse("up"),
-			"dummy-binary1": parse("up"),
-			"dummy-binary2": parse("up"),
-			"pirate-birthday": parse("up"),
-			"pirate-birthday-new": parse("up"),
+			"asocialnetwork": parse("up"),
+			"bollwerk": parse("up"),
+			"phreaking": parse("up"),
+			"yvm": parse("up"),
+			"granulizer": parse("up"),
+			"oldschool": parse("up"),
+			"steinsgate": parse("up"),
 		}
 	}
 	data_teams[t["id"]] = data_team
