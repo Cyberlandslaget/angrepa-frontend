ac56011d91c8070beab3550a94a8a2a49af52007
Jonny Ngo Luong <shirajuki00@gmail.com>
2023-10-02 01:42:16 +0200

    Fix blacklist becoming string issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,24 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    million.vite({ auto: true }),
-    react(),
-    tsconfigPaths(),
-    svgr({ svgrOptions: { titleProp: true } }),
-  ],
+export default defineConfig(({ command, mode }) => {
+  console.log(mode);
+  if (command === 'serve') {
+    return {
+      plugins: [
+        react(),
+        tsconfigPaths(),
+        svgr({ svgrOptions: { titleProp: true } }),
+      ],
+    };
+  } else {
+    return {
+      plugins: [
+        million.vite({ auto: true }),
+        react(),
+        tsconfigPaths(),
+        svgr({ svgrOptions: { titleProp: true } }),
+      ],
+    };
+  }
 });
